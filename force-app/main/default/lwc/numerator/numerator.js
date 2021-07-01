e07ea1a5a952260747e80ca7df0b068462e6273f
Desu Desu <desudesu410@gmail.com>
2021-07-01 15:37:34 +0300

    feat: add message-channel

--- force-app/main/default/lwc/numerator/numerator.js
+++ force-app/main/default/lwc/numerator/numerator.js
@@ -1,7 +1,18 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, api } from 'lwc';
 
 export default class Numerator extends LightningElement {
-  counter = 0;
+  //@api counter = 0;
+  _currentCount = 0;
+  priorCount = 0;
+
+  @api get counter() {
+    return this._currentCount;
+  }
+  
+  set counter(value) {
+    this.priorCount = this._currentCount;
+    this._currentCount = value;
+  }
 
   handleIncrement() {
     this.counter++;
@@ -19,5 +30,9 @@ export default class Numerator extends LightningElement {
   handleDivide(event) {
     const divisor = event.detail;
     this.counter *= 1 / divisor;
+  }
+
+  @api maximizeCounter() {
+    this.counter += 1000000;
   }
 }
